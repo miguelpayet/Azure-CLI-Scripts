1cb06811d4ce316dfb693c6cc55cbff28126ebd6
Miguel Payet <miguelpayet@gmail.com>
2018-11-16 09:52:20 -0500

    cambios

--- lista-costos.ps1
+++ lista-costos.ps1
@@ -1,35 +1,30 @@
 write-host('hola')
 
-function Procesar-Linea($consumo) {
-    $fieldsConsumo = $consumo.split("`t")
-    if ($fieldsConsumo[11].indexOf("/") -eq -1) {
-        $grupo = $fieldsConsumo[11] 
-    } else {
-        $fieldsInstance = $fieldsConsumo[11].split("/")
-        $grupo = $fieldsInstance[4]
-    }
-    Write-Host($fieldsAccount[0],$fieldsConsumo[0],$fieldsConsumo[1],$fieldsConsumo[2],$fieldsConsumo[3],$fieldsConsumo[4],$fieldsConsumo[5],$fieldsConsumo[6],$fieldsConsumo[7],$fieldsConsumo[8],$fieldsConsumo[9],$fieldsConsumo[10],$grupo) -Separator ","
-}
-
 $accounts = az account list --query '[].[name, id]' --output tsv
-Start-Transcript -LiteralPath "test.txt"
-write-host('cuenta','accountName','instanceLocation','instanceName','currency','pretaxCost','product','consumedService','subscriptionName','usageStart','usageEnd','billingPeriodId','resourceGroup') -Separator ","
+Start-Transcript -LiteralPath "test.csv"
+write-host('suscripción', 'cuenta', 'instanceLocation','instanceName','currency','pretaxCost','product','consumedService','subscriptionName','usageStart','usageEnd','billingPeriodId','resourceGroup') -Separator ","
 for ($i = 0; $i -lt $accounts.Length; $i++) {
     $fieldsAccount = $accounts[$i].split("`t")
     az account set --subscription $fieldsAccount[1] 
-    $consumo = az consumption usage list --query [].[accountName,instanceLocation,instanceName,currency,pretaxCost,product,consumedService,subscriptionName,usageStart,usageEnd,billingPeriodId,instanceId] --output tsv --start-date '2018-01-01' --end-date '2018-01-31'
+    $consumo = az consumption usage list --query [].[accountName,instanceLocation,instanceName,currency,pretaxCost,product,consumedService,subscriptionName,usageStart,usageEnd,billingPeriodId,instanceId] --start-date '2018-09-01' --end-date '2018-11-16' --output tsv
     if ($consumo) {
-        if ($consumo.GetType().fullname -eq "System.String") {
-            Procesar-Linea($consumo)
-        } else {
-            for ($j = 0; $j -lt $consumo.Length; $j++) {
-                if ($consumo[$j]) {
-                    Procesar-Linea($consumo[$j])                   
-                }
-            }
+        if ($consumo.GetType().fullname -ne "System.String") {
+            foreach ($reg in $consumo) {
+                #Write-Host($reg)
+                $arr = $reg.split("`t")
+                if ($arr[11].indexOf("/") -eq -1) {
+                    $grupo = $arr[11] 
+                } else {
+                    $instance = $arr[11].split("/")
+                    $grupo = $instance[4]
         }
-    } else {
-        Write-Host($fieldsAccount[0],"","","","",0,"","","","","","","") -Separator ","
+                Write-Host($fieldsAccount[0], $arr[0], $arr[1],$arr[2],$arr[3],$arr[4],$arr[5],$arr[6],$arr[7],$arr[8],$arr[9],$arr[10],$grupo) -Separator ","
+            }
+        } else {
+            Write-Host($fieldsAccount[0], "linea en blanco") -Separator ""
+        }
     }
 }
 Stop-Transcript
+
+Write-Host("bye")
